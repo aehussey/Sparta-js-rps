3eeb4685d0b2659cd4df711d03687e3930ff1767
Tech-A06 <tech-a06@Tech-A06s-MacBook-Air.local>
2019-04-10 09:28:41 +0100

    Fixed some grammar and made the readout tell you what the computer had picked

--- main.js
+++ main.js
@@ -6,9 +6,10 @@ function startGame() {
 
 function playGame() {
   var humanChoice = prompt("What will it be? Rock, Paper or Scissors?");
-    if (humanChoice != "rock" || humanChoice != "paper" || humanChoice != "scissors")
-    {var humanChoice = prompt("What will it be? Rock, Paper or Scissors? - Make sure you spell it correctly!");}
-  return humanChoice;
+    if (humanChoice.toLowerCase() != "rock" && humanChoice != "paper" && humanChoice != "scissors")
+    {var humanChoice = prompt("What will it be? Rock, Paper or Scissors? - Make sure you spell it correctly!");
+    return humanChoice}
+  else {return humanChoice;}
 }
 
 function compChoice() {
@@ -16,6 +17,20 @@ function compChoice() {
   max = Math.floor(3);
 
   return Math.floor(Math.random() * (3 - 0)) + 0;
+
+}
+
+
+function compWord (compNumb) {
+  var cmpWord;
+  if (compNumb == 0)
+    {cmpWord = "rock"}
+  else if (compNumb == 1)
+    {cmpWord = "paper"}
+  else if (compNumb == 2)
+    {cmpWord = "scissors"}
+    console.log(cmpWord);
+  return cmpWord;
 
 }
 
@@ -34,23 +49,26 @@ function humChoiceToNumber(humChoice) {
 
 function winner(humNumb, compNumb) {
 
+
+
   if (humNumb == compNumb) {
-    alert("That was a tie")
+    alert("The computer chose " + compWord(compNumb) + ". That was a tie.")
   }
 
   else if ((compNumb == 0 && humNumb == 1) || (compNumb == 1 && humNumb == 2) || (compNumb == 2 && humNumb == 0)) {
-    alert("Computer chose You won this round!")
+    alert("The computer chose " + compWord(compNumb) + ". You won this round!")
     humPoints ++;
   }
   else {
-    alert("The computer won this round")
+    alert("The computer chose " + compWord(compNumb) + ". The computer won this round.")
     compPoints ++
   }
   currentScore(humPoints, compPoints);
+
 }
 
 function currentScore(humPoints, compPoints) {
-  alert("The computer has " + compPoints + " and you have " + humPoints + " points")
+  alert("The computer has " + compPoints + " and you have " + humPoints + " points.")
 }
 
 function gameDuration() {
